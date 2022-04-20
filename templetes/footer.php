9f839a5f0a40c1c9ded808b474beb49c6d7a9211
Md. Sakib Ul Islam <63756507+mdsakibulislamm@users.noreply.github.com>
2022-04-21 03:09:45 +0600

    Add files via upload

---
+++ templetes/footer.php
@@ -0,0 +1,23 @@
+<!-- Footer-->
+        <footer class="footer text-center bg-secondary">
+            <div class="container">
+                <div class="row">
+                    <!-- Footer Location-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Location</h4>
+                        <p class="lead mb-0">DHAKA<br />BANASREE, BLOCK G</p>
+                    </div>
+                    <!-- Footer Social Icons-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">DOTBLOG WEB</h4>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#"><i class="fab fa-fw fa-facebook-f"></i></a><a class="btn btn-outline-light btn-social mx-1" href="#"><i class="fab fa-fw fa-twitter"></i></a><a class="btn btn-outline-light btn-social mx-1" href="#"><i class="fab fa-fw fa-linkedin-in"></i></a><a class="btn btn-outline-light btn-social mx-1" href="#"><i class="fab fa-fw fa-dribbble"></i></a>
+                    </div>
+                    <!-- Footer About Text-->
+                    <div class="col-lg-4">
+                        <h4 class="text-uppercase mb-4">About Blogger</h4>
+                        <p class="lead mb-0">Blogger web is a free to use. All blogger of the Bangladesh can post here. To make this types of web knock me...<a href="https://www.facebook.com/md.sakibulislam.94/"> Facebook</a>.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        
